20033cddbc1cd5c2d9fc6f99683f1ac141a1426e
shriram2007721 <swethashriram123@gmail.com>
2026-09-10 14:25:21 +0530

    Create InvalidRequestException.java

---
+++ src/main/java/com/emergency/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package com.emergency.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) { 
+        super(message); 
+    }
+}
